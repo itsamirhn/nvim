0b57de473ee1072f4c2a99d7efbe747bbbbe8028
Amir Hosseini <awmirhn@gmail.com>
2026-06-07 15:07:47 +0200

    feat: add treesitter with rust and python parsers

--- init.lua
+++ init.lua
@@ -7,12 +7,14 @@ vim.pack.add({
   'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/stevearc/oil.nvim',
   'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
   { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('*') },
 })
 
 require('plugins.fzf-lua')
 require('plugins.oil')
 require('plugins.gitsigns')
+require('plugins.treesitter')
 require('plugins.blink-cmp')
 
 vim.lsp.enable({ 'rust_analyzer', 'pyright' })
